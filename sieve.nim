75a3f30fa87a70fe98619ade3240faca280cadc4
Tyler Eaves <teaves@reflector.com>
2013-12-16 09:41:06 -0500

    Really working sieve

--- sieve.nim
+++ sieve.nim
@@ -3,31 +3,37 @@ import math
 proc primes*(target: int): seq[int] =
   let sz = int(ceil(target/2))
   var sieve = newseq[bool](sz)
-  for x in 0..sz-1:
-    sieve[x] = false
 
   let maxv = target
   let stop = int(sqrt(float(target)))
   var n = 3
   var accum = newseq[int]()
   accum.add(2)
+  sieve[0] = true
   while n < stop:
+
     let start = n*n
     for x in countup(start,target,n):
-      sieve[(x-3) /% 2] = true 
+      if x mod 2 != 0:
+        sieve[(x-3) /% 2] = true 
+
     let p = sieve.find(false)
+ 
     if p == -1:
       break
+    if n >= stop:
+      break
+    
     sieve[p] = true
     accum.add((p*2)+3)
     n = (p*2)+3
-    if n >= stop:
-      break
+    
   for x in 0..len(sieve)-1:
+   
     if sieve[x] == false and ((x*2)+3) < target:
       accum.add((x*2)+3)
   return accum
 when isMainModule:
-  echo(repr(primes(1000)))
+  echo(repr(primes(1000000)))
 
 
