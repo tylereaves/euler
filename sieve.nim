5696839dc6ed1e21b895c0dbbe49c611971196f6
Tyler Eaves <teaves@reflector.com>
2013-12-16 09:25:45 -0500

    Working sieve

--- sieve.nim
+++ sieve.nim
@@ -1,29 +1,33 @@
 import math
 
 proc primes*(target: int): seq[int] =
-  #Not fully optimal (it checks evens)
   let sz = int(ceil(target/2))
   var sieve = newseq[bool](sz)
+  for x in 0..sz-1:
+    sieve[x] = false
+
   let maxv = target
   let stop = int(sqrt(float(target)))
-  var n = 2
-  result.add(2)
+  var n = 3
+  var accum = newseq[int]()
+  accum.add(2)
   while n < stop:
     let start = n*n
     for x in countup(start,target,n):
-      sieve[(x-3) /% 2] = true
-
+      sieve[(x-3) /% 2] = true 
     let p = sieve.find(false)
     if p == -1:
       break
     sieve[p] = true
-    result.add((p*2)+3)
-    n = p
+    accum.add((p*2)+3)
+    n = (p*2)+3
     if n >= stop:
       break
   for x in 0..len(sieve)-1:
-    result.add((x*2)+3)
-
-
+    if sieve[x] == false and ((x*2)+3) < target:
+      accum.add((x*2)+3)
+  return accum
+when isMainModule:
+  echo(repr(primes(1000)))
 
 
